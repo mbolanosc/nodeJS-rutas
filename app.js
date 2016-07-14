a5dbb1ac322f0a3a1a4b9839f09dd134030b02c1
mbolanosc <mrybc96@gmail.com>
2016-07-13 19:58:15 -0600

    rutas como antes

---
+++ app.js
@@ -0,0 +1,21 @@
+//manera de incluir librerias
+//ya utiliza un http server entonces nada mas se llama
+var http = require('http');
+var Router = require('node-router');
+
+var router = Router();
+var route = router.push;
+//rutas:
+route('GET', '/', function(request,response){
+	console.log('llamada al index.');
+	response.send('Hola mundo de rutas yeyyy!');
+})
+route('GET' , '/hola', function(request,response){
+	console.log('llamada al /hola');
+	response.send('error');
+})
+
+var server = http.createServer(router);
+server.listen(8000,function(){
+	console.log('Escuchando puerto 8000');
+});
